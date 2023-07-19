02907ee8c5bf7d380f5ec11c9c9f24b248b609bf
Benoit Mirete <ben@mirete.fr>
2023-07-19 20:28:56 +0200

    feat: configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CharactersModule } from './characters/characters.module';
 import { PingModule } from './ping/ping.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmExModule } from './config/orm/typeorm-ex.module';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
-  imports: [CharactersModule, PingModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    TypeOrmExModule.forCustomRepository([]),
+    CharactersModule,
+    PingModule,
+  ],
   controllers: [],
   providers: [],
 })
