75b621bd62cd3e1e6452b38f523a67d863f0ab87
Benoit Mirete <ben@mirete.fr>
2023-07-20 08:31:44 +0200

    feat: enable swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express'; // Add this line
@@ -17,6 +18,14 @@ async function bootstrap() {
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Death May Die API')
+    .setDescription('The Death May Die API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.SERVER_PORT;
   await app.listen(port);
